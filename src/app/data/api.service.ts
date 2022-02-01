f536cf1d723455b184e4019c1641ae3eed1897ee
sanjeevagrl <sanjeevagrl@gmail.com>
2022-02-01 08:09:06 +0000

    sharereplay

--- src/app/data/api.service.ts
+++ src/app/data/api.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { shareReplay } from 'rxjs/operators';
 import { Product } from '../data/product';
 import { Customer } from './customer';
 import { Order } from './order';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-  products$ = this.http.get<Product[]>('api/products');
-  customers$ = this.http.get<Customer[]>('api/customers');
+  products$ = this.http.get<Product[]>('api/products').pipe(shareReplay());
+  customers$ = this.http.get<Customer[]>('api/customers').pipe(shareReplay());
   orders$ = this.http.get<Order[]>('api/orders');
 
   constructor(private http: HttpClient) {}
